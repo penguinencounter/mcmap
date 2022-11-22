09ae365542a89f61992efb35b10749c1c57aff13
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-11-22 08:46:49 -0800

    improve speed

--- scanner/task_masscan.py
+++ scanner/task_masscan.py
@@ -4,6 +4,10 @@
 import ipaddress
 import json
 import os
+import shlex
+import subprocess
+import threading
+import time
 import typing
 
 from scanner.eta import ETA
@@ -35,19 +39,24 @@ def dump_block(block: typing.List[ipaddress.IPv4Address], filename: str):
 
 
 def compress(block: typing.List[ipaddress.IPv4Address]) -> typing.List[ipaddress.IPv4Network]:
-    print(f'\rconverting addresses to networks...'.ljust(100), end='', flush=True)
-    subnets: typing.List[ipaddress.IPv4Network] = [ipaddress.ip_network((addr, 32), False) for addr in block]
+    subnets: typing.List[typing.Union[ipaddress.IPv4Network, ipaddress.IPv4Address]] = block
 
     for mask in range(31, -1, -1):
-        print(f'\rcompressing subnets ({32 - mask}/32)...'.ljust(100), end='', flush=True)
-
         # sort the subnets by their first address
-        subnets.sort(key=lambda x: x.network_address.packed)
+        subnets.sort(key=lambda x: x.network_address.packed if isinstance(x, ipaddress.IPv4Network) else x.packed)
         # merge subnets together if they are contiguous
         merged = []
         # take pairs
         for i in range(0, len(subnets), 2):
             # if the first subnet is contiguous with the next one, merge them
+            if isinstance(subnets[i], ipaddress.IPv4Address):
+                if mask != 31:
+                    raise NotImplementedError('mask is not 31 and we have an address')
+                if i + 1 < len(subnets) and subnets[i] + 1 == subnets[i + 1]:
+                    merged.append(ipaddress.IPv4Network((subnets[i], mask), False))
+                else:
+                    merged.append(ipaddress.IPv4Network((subnets[i], 32)))
+                continue
             if i + 1 < len(subnets) and subnets[i].broadcast_address + 1 == subnets[i + 1].network_address:
                 merged.append(ipaddress.ip_network((subnets[i].network_address, mask), False))
             else:
@@ -59,7 +68,7 @@ def compress(block: typing.List[ipaddress.IPv4Address]) -> typing.List[ipaddress
 
 
 def split_block(subnets: typing.List[ipaddress.IPv4Network], target_mask: int = 24) -> typing.List[
-                typing.List[ipaddress.IPv4Address]]:
+    typing.List[ipaddress.IPv4Address]]:
     """Split a list of subnets into smaller blocks, based on the target mask"""
     building = []
     addr_count = 2 ** (32 - target_mask)
@@ -85,7 +94,8 @@ def split_block(subnets: typing.List[ipaddress.IPv4Network], target_mask: int = 
                     blocks_done += 1
                     rem = estimated_steps - blocks_done
                     print(f'\rabout {round(total_done / total_estimate * 100, 2)}% done;'
-                          f' Estimated {eta.get(rem)} remaining (done {blocks_done} -> {total_done}) (currently in {net})'.ljust(100),
+                          f' Estimated {eta.get(rem)} remaining (done {blocks_done} -> {total_done}) (currently in {net})'.ljust(
+                        100),
                           end='', flush=True)
                     yield building
                     building = []
@@ -109,6 +119,36 @@ def write_progress_backup(data: int):
         f.write(str(data))
 
 
+MASSCAN_PARAMS = shlex.split("-p 25565")
+batch = []
+stop = False
+thread_state = {"main": "run"}
+
+
+def async_file_writer(name: typing.Union[str, int]):
+    name = f"w{name}"
+    thread_state[name] = "run"
+    while not stop:
+        while len(batch) == 0:
+            thread_state[name] = "idle"
+            time.sleep(0.1)
+        thread_state[name] = "run"
+        addrs = batch.pop(0)
+        idx = 0
+        while os.path.exists(f'tasks/{idx}.masscan'):
+            idx += 1
+        thread_state[name] = "comp"
+        compr = compress(addrs)
+        thread_state[name] = "write"
+        write_task_file(compr, idx)
+    del thread_state[name]
+
+
+def write_task_file(subnets: typing.List[ipaddress.IPv4Network], idx: int):
+    with open(f'tasks/{idx}.masscan', 'wb') as f:
+        f.write(subprocess.check_output(['masscan', '--echo'] + MASSCAN_PARAMS + list(map(lambda x: x.exploded, subnets))))
+
+
 def read_progress_backup() -> int:
     if os.path.exists('continue.bak'):
         with open('continue.bak', 'r') as f:
@@ -116,7 +156,15 @@ def read_progress_backup() -> int:
     return 0
 
 
+def display_status():
+    print('\r', end='', flush=True)
+    for name, state in thread_state.items():
+        print(f'{name}: {state}'.ljust(12), end='')
+    print('', end='', flush=True)
+
+
 def build_blocks(backup_between: int = 50, continuation: int = 0):
+    global stop, batch
     conf = load_config()
     target = ipaddress.ip_network(conf['target'], False)
     block_mask = conf['block_mask']
@@ -130,8 +178,8 @@ def build_blocks(backup_between: int = 50, continuation: int = 0):
     print(f'Using strategy: {conf["blocking_strategy"]}')
     blocks = strategy(frags, block_mask)
 
-    if not os.path.exists('blocks'):
-        os.mkdir('blocks')
+    if not os.path.exists('tasks'):
+        os.mkdir('tasks')
 
     ctr = 0
 
@@ -143,11 +191,28 @@ def build_blocks(backup_between: int = 50, continuation: int = 0):
         ctr += 1
 
     print(f'\nstarting...')
-    for i, a in enumerate(blocks):
-        ctr += 1
-        if ctr % backup_between == 0:
-            write_progress_backup(i)
-        print("\n", compress(a))
+    threads = []
+    THREAD_COUNT = 5
+    MAX_BATCH = 10
+    for i in range(THREAD_COUNT):
+        t = threading.Thread(target=async_file_writer, args=(i,))
+        t.start()
+        threads.append(t)
+
+    try:
+        for i, a in enumerate(blocks):
+            ctr += 1
+            if ctr % backup_between == 0:
+                write_progress_backup(i)
+            while len(batch) > MAX_BATCH:
+                thread_state['main'] = 'wait'
+                display_status()
+                time.sleep(0.1)
+            thread_state['main'] = 'run'
+            display_status()
+            batch.append(a)
+    finally:
+        stop = True
 
 
 if __name__ == '__main__':
