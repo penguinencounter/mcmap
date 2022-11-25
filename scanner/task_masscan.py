313bee97ae47d29a14eeaccd591d0439174efa74
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-11-25 15:44:01 -0800

    And it really do be zooming

--- scanner/task_masscan.py
+++ scanner/task_masscan.py
@@ -3,18 +3,20 @@
 # Scan blocks, compress em, and keep a status file
 import ipaddress
 import json
+import math
+import multiprocessing
 import os
 import shlex
 import subprocess
-import threading
 import time
 import typing
+from math import floor
 
 from scanner.eta import ETA
 
 
 def process_exclusions(subnet: ipaddress.IPv4Network, exclusions: typing.List[str]) -> typing.List[
-    ipaddress.IPv4Network]:
+        ipaddress.IPv4Network]:
     fragmented: typing.List[ipaddress.IPv4Network] = [subnet]
     for exclusion in exclusions:
         print('\r', end='', flush=True)
@@ -38,25 +40,17 @@ def dump_block(block: typing.List[ipaddress.IPv4Address], filename: str):
             f.write(addr.packed + b' ')
 
 
-def compress(block: typing.List[ipaddress.IPv4Address]) -> typing.List[ipaddress.IPv4Network]:
-    subnets: typing.List[typing.Union[ipaddress.IPv4Network, ipaddress.IPv4Address]] = block
+def compress(block: typing.List[ipaddress.IPv4Network]) -> typing.List[ipaddress.IPv4Network]:
+    subnets: typing.List[typing.Union[ipaddress.IPv4Network]] = block
 
     for mask in range(31, -1, -1):
         # sort the subnets by their first address
-        subnets.sort(key=lambda x: x.network_address.packed if isinstance(x, ipaddress.IPv4Network) else x.packed)
+        subnets.sort(key=lambda x: x.network_address.packed)
         # merge subnets together if they are contiguous
         merged = []
         # take pairs
         for i in range(0, len(subnets), 2):
             # if the first subnet is contiguous with the next one, merge them
-            if isinstance(subnets[i], ipaddress.IPv4Address):
-                if mask != 31:
-                    raise NotImplementedError('mask is not 31 and we have an address')
-                if i + 1 < len(subnets) and subnets[i] + 1 == subnets[i + 1]:
-                    merged.append(ipaddress.IPv4Network((subnets[i], mask), False))
-                else:
-                    merged.append(ipaddress.IPv4Network((subnets[i], 32)))
-                continue
             if i + 1 < len(subnets) and subnets[i].broadcast_address + 1 == subnets[i + 1].network_address:
                 merged.append(ipaddress.ip_network((subnets[i].network_address, mask), False))
             else:
@@ -68,9 +62,8 @@ def compress(block: typing.List[ipaddress.IPv4Address]) -> typing.List[ipaddress
 
 
 def split_block(subnets: typing.List[ipaddress.IPv4Network], target_mask: int = 24) -> typing.List[
-    typing.List[ipaddress.IPv4Address]]:
+        typing.List[ipaddress.IPv4Network]]:
     """Split a list of subnets into smaller blocks, based on the target mask"""
-    building = []
     addr_count = 2 ** (32 - target_mask)
     total_estimate = sum(net.num_addresses for net in subnets)
     estimated_steps = total_estimate / addr_count
@@ -81,27 +74,30 @@ def split_block(subnets: typing.List[ipaddress.IPv4Network], target_mask: int = 
     print('sequential block splitter: {} estimated total blocks'.format(estimated_steps))
     print('sequential block splitter: on-demand ready')
     yield None
-    for net in subnets:
-        can_fill = addr_count - len(building)
-        if can_fill > net.num_addresses:
-            building.extend(net.hosts())
-        else:
-            iterable = net.hosts()
-            for host in iterable:
-                if len(building) == addr_count:
-                    total_done += len(building)
-                    eta.step()
-                    blocks_done += 1
-                    rem = estimated_steps - blocks_done
-                    print(f'\rabout {round(total_done / total_estimate * 100, 2)}% done;'
-                          f' Estimated {eta.get(rem)} remaining (done {blocks_done} -> {total_done}) (currently in {net})'.ljust(
-                        100),
-                          end='', flush=True)
-                    yield building
-                    building = []
-                building.append(host)
-    print('sequential block splitter: done')
-    yield building
+    while True:
+        parts = []
+        fill = 0
+        while fill < addr_count:
+            if len(subnets) == 0:
+                yield parts
+                return
+            remaining = addr_count - fill
+            next_block_up = subnets.pop(0)
+            max_next_mask = 32 - floor(math.log2(remaining))
+            if next_block_up.prefixlen >= max_next_mask:
+                parts.append(next_block_up)
+                fill += next_block_up.num_addresses
+            else:
+                # split the block into parts
+                new_blocks = list(next_block_up.subnets(new_prefix=max_next_mask))
+                if len(new_blocks) > 1:  # safety check
+                    subnets = new_blocks[1:] + subnets
+                parts.append(new_blocks[0])
+                fill += new_blocks[0].num_addresses
+        total_done += fill
+        blocks_done += 1
+        eta.step()
+        yield parts
 
 
 def load_config() -> dict:
@@ -123,36 +119,39 @@ MASSCAN_PARAMS = shlex.split("-p 25565")
 batch = []
 stop = False
 thread_state = {"main": "run"}
+I_AM_A = os.getpid()
 
 
-def async_file_writer(name: typing.Union[str, int]):
-    name = f"w{name}"
-    thread_state[name] = "run"
-    while not stop:
-        while len(batch) == 0:
-            thread_state[name] = "idle"
-            time.sleep(0.1)
-        thread_state[name] = "run"
-        addrs = batch.pop(0)
-        idx = 0
-        while os.path.exists(f'tasks/{idx}.masscan'):
-            idx += 1
-        thread_state[name] = "comp"
-        compr = compress(addrs)
-        thread_state[name] = "write"
-        write_task_file(compr, idx)
-    del thread_state[name]
+def runner(q: multiprocessing.Queue):
+    score = 0
+    print(f'[{I_AM_A}] Starting up...', flush=True)
+    while True:
+        work = q.get(True)
+        if work == "STOP":
+            print(f'\r[{I_AM_A}] ({score}) Stopping!'.ljust(100), end='', flush=True)
+            break
+        print(f'\r[{I_AM_A}] ({score}) Starting to work on job #{score + 1}'.ljust(100), end='', flush=True)
+        compr = compress(work)
+        write_task_file(compr)
+        print(f'\r[{I_AM_A}] ({score}) Finished job #{score + 1}'.ljust(100), end='', flush=True)
+        score += 1
 
 
-def write_task_file(subnets: typing.List[ipaddress.IPv4Network], idx: int):
+def write_task_file(subnets: typing.List[ipaddress.IPv4Network]):
+    idx = 0
+    while os.path.exists(f'tasks/{idx}.masscan'):
+        idx += 1
     with open(f'tasks/{idx}.masscan', 'wb') as f:
-        f.write(subprocess.check_output(['masscan', '--echo'] + MASSCAN_PARAMS + list(map(lambda x: x.exploded, subnets))))
+        f.write(
+            subprocess.check_output(['masscan', '--echo'] + MASSCAN_PARAMS + list(map(lambda x: x.exploded, subnets))))
 
 
 def read_progress_backup() -> int:
-    if os.path.exists('continue.bak'):
-        with open('continue.bak', 'r') as f:
-            return int(f.read())
+    if os.path.exists('tasks'):
+        c = 0
+        while os.path.exists(f'tasks/{c}.masscan'):
+            c += 1
+        return c
     return 0
 
 
@@ -177,7 +176,10 @@ def build_blocks(backup_between: int = 50, continuation: int = 0):
     strategy = STRATEGIES[conf['blocking_strategy']]
     print(f'Using strategy: {conf["blocking_strategy"]}')
     blocks = strategy(frags, block_mask)
-
+    next(blocks)
+    for i, v in enumerate(blocks):
+        print(f'\r{i}, {sum(map(lambda x: x.num_addresses, v))}'.ljust(50), end='', flush=True)
+    return
     if not os.path.exists('tasks'):
         os.mkdir('tasks')
 
@@ -191,30 +193,41 @@ def build_blocks(backup_between: int = 50, continuation: int = 0):
         ctr += 1
 
     print(f'\nstarting...')
-    threads = []
-    THREAD_COUNT = 5
-    MAX_BATCH = 10
-    for i in range(THREAD_COUNT):
-        t = threading.Thread(target=async_file_writer, args=(i,))
-        t.start()
-        threads.append(t)
+    THREAD_COUNT = 15
+    procs = []
+    work_pool = multiprocessing.Queue(50)  # specify max size to limit mem usage
+    for _ in range(THREAD_COUNT):
+        p = multiprocessing.Process(target=runner, args=(work_pool,))
+        p.start()
+        procs.append(p)
 
     try:
         for i, a in enumerate(blocks):
             ctr += 1
             if ctr % backup_between == 0:
                 write_progress_backup(i)
-            while len(batch) > MAX_BATCH:
-                thread_state['main'] = 'wait'
-                display_status()
-                time.sleep(0.1)
-            thread_state['main'] = 'run'
-            display_status()
-            batch.append(a)
+            print(f'\r[{I_AM_A}] Job added...'.ljust(100), end='', flush=True)
+            while work_pool.full():
+                time.sleep(1)
+            work_pool.put(a)
     finally:
-        stop = True
+        work_pool.put("STOP")
+        t = 0
+        TIMEOUT = 10
+        last = time.time()
+        while any(p.is_alive() for p in procs):
+            t += (time.time() - last)
+            last = time.time()
+            time.sleep(0.01)
+            print(f'\r[{I_AM_A}] Waiting for threads to stop...'.ljust(100), end='', flush=True)
+            if t > TIMEOUT:
+                print(f'\r[{I_AM_A}] Timeout waiting for threads to stop!'.ljust(100), end='', flush=True)
+                for p in procs:
+                    p.kill()
+        print(f'\r[{I_AM_A}] Stopped.'.ljust(100), end='\n', flush=True)
 
 
 if __name__ == '__main__':
+    I_AM_A = f'{I_AM_A} (main)'
     prog = read_progress_backup()
     build_blocks(continuation=prog)
